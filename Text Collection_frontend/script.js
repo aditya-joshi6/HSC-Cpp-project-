7d029fc4712d50d455eb6346c40a7fbb81a06404
karan2510 <76568108+karan2510@users.noreply.github.com>
2024-04-21 21:53:57 +0530

    Text Collection Form Frontend

---
+++ Text Collection_frontend/script.js
@@ -0,0 +1,27 @@
+function sendText() {
+    var text = document.getElementById("textInput").value; 
+    var base64EncodedText = btoa(text);
+
+    // Configure the request
+    fetch("http://localhost:8081", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/base64" 
+      },
+      body: base64EncodedText // Send Base64 encoded
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+    
+      console.log(data);
+    })
+    .catch(error => {
+      // handle errors 
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
