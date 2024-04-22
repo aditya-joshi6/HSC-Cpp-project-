a310f4165635f541e430aa560eeb3564fa5833a8
karan2510 <76568108+karan2510@users.noreply.github.com>
2024-04-22 18:23:46 +0530

    Removed Hardcoded Decoder and textDecode.h

--- Combine_IPC_Server/CombineServer_IPC.cpp
+++ Combine_IPC_Server/CombineServer_IPC.cpp
@@ -17,8 +17,9 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
-#include "D:\repos\inference\b64decoder.h"
+#include "b64decoder.h"
 #include <cstring>
+#include "b64decodertext.h"
 
 // Mutex for thread safety
 std::mutex mtx;
@@ -29,84 +30,7 @@ using boost::interprocess::read_write;
 using boost::interprocess::open_or_create;
 using namespace boost::interprocess;
 
-// Base64 Alphabet char function
-int base_code(char chr) {
-    if (chr == '=') {
-        return NULL;
-    }
 
-    char alphabet[] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' };
-
-    int len = sizeof(alphabet) / sizeof(char);
-
-    for (int i = 0; i < len; i++) {
-        if (chr == alphabet[i]) {
-            return i;
-        }
-    }
-}
-
-std::string decode(std::string text) {
-    // Declaration
-    std::vector<int> bits;
-
-    // Get each character Base64 code and put it in bits vector
-    for (int i = 0; i < text.length(); i++) {
-        bits.push_back(base_code(text[i]));
-    }
-
-    // Turn Base64 code into binary
-    std::string binaryString;
-    for (int i = 0; i < bits.size(); i++) {
-        std::string revString;
-
-        // Calculate Binary
-        while (bits[i] > 0) {
-            int remain = bits[i] % 2;
-            revString += std::to_string(remain);
-            bits[i] /= 2;
-        }
-
-        // If Binary length is less than 6, put a 0 behind it
-        while (revString.length() < 6) {
-            revString += "0";
-        }
-
-        // Reverse the string so it would match before we divide it to a group of 8
-        for (int j = revString.length() - 1; j >= 0; j--) {
-            binaryString += revString[j];
-        }
-    }
-    // Clear bits vector so we could use it later
-    bits.clear();
-    // Divide the binary into a group with 8 elements
-    std::vector<std::string> binaryTemp;
-    while (binaryString.length() > 0) {
-        std::string temp = binaryString.substr(0, 8);
-        binaryTemp.push_back(temp);
-        binaryString.erase(0, 8);
-    }
-    // Convert from Binary to ASCII code
-    for (int i = 0; i < binaryTemp.size(); i++) {
-        int result = 0;
-        for (int j = binaryTemp[i].length() - 1; j >= 0; j--) {
-            if (binaryTemp[i].at(j) % 2 == 1) {
-                result += pow(2, binaryTemp[i].length() - 1 - j);
-            }
-        }
-        // Put the ASCII into the bits vector
-        bits.push_back(result);
-    }
-    // We convert the ASCII to char
-    std::string output;
-    for (int i = 0; i < bits.size(); i++) {
-        output += char(bits[i]);
-    }
-    // Return the output
-    return output;
-}
 void handleFileUpload(tcp::socket& socket) {
    boost::beast::flat_buffer buffer;
     http::request<http::string_body> request;
